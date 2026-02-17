18aeef4613edc1c23d9aee5d532399aab6e1ff78
arnab <arnhazra@gmail.com>
2026-02-17 08:24:13 +0530

    Publish App

--- services/core-service/src/apps/debttrack/debttrack.module.ts
+++ services/core-service/src/apps/debttrack/debttrack.module.ts
@@ -1,16 +1,39 @@
 import { Module } from "@nestjs/common"
-import { DebtModule } from "./debt.module"
 import { EntityModule } from "@/shared/entity/entity.module"
 import { config } from "@/config"
 import { AppsDbConnectionMap } from "@/shared/entity/entity-db-connection.map"
+import { DebtService } from "./debt.service"
+import { DebtController } from "./debt.controller"
+import { CqrsModule } from "@nestjs/cqrs"
+import { Debt, DebtSchema } from "./schemas/debt.schema"
+import { DebtRepository } from "./debt.repository"
+import { CreateDebtCommandHandler } from "./commands/handler/create-debt.handler"
+import { DeleteDebtCommandHandler } from "./commands/handler/delete-debt.handler"
+import { FindDebtByIdQueryHandler } from "./queries/handler/find-debt-by-id.handler"
+import { UpdateDebtCommandHandler } from "./commands/handler/update-debt.handler"
+import { FindDebtsByUserQueryHandler } from "./queries/handler/find-debt-by-user.handler"
 
 @Module({
   imports: [
+    CqrsModule,
     EntityModule.forRoot(
       config.APPS_DATABASE_URI,
       AppsDbConnectionMap.DebtTrack
     ),
-    DebtModule,
+    EntityModule.forFeature(
+      [{ name: Debt.name, schema: DebtSchema }],
+      AppsDbConnectionMap.DebtTrack
+    ),
+  ],
+  controllers: [DebtController],
+  providers: [
+    DebtService,
+    DebtRepository,
+    CreateDebtCommandHandler,
+    UpdateDebtCommandHandler,
+    DeleteDebtCommandHandler,
+    FindDebtsByUserQueryHandler,
+    FindDebtByIdQueryHandler,
   ],
 })
 export class DebtTrackModule {}
--- services/core-service/src/apps/planner/planner.module.ts
+++ services/core-service/src/apps/planner/planner.module.ts
@@ -1,13 +1,32 @@
 import { Module } from "@nestjs/common"
-import { EventModule } from "./event.module"
 import { EntityModule } from "@/shared/entity/entity.module"
 import { config } from "@/config"
 import { AppsDbConnectionMap } from "@/shared/entity/entity-db-connection.map"
+import { CqrsModule } from "@nestjs/cqrs"
+import { EventService } from "./event.service"
+import { EventController } from "./event.controller"
+import { Event, EventSchema } from "./schemas/event.schema"
+import { EventRepository } from "./event.repository"
+import { CreateEventCommandHandler } from "./commands/handler/create-event.handler"
+import { DeleteEventCommandHandler } from "./commands/handler/delete-event.handler"
+import { FindEventsByUserQueryHandler } from "./queries/handler/find-event-by-user.handler"
 
 @Module({
   imports: [
+    CqrsModule,
     EntityModule.forRoot(config.APPS_DATABASE_URI, AppsDbConnectionMap.Planner),
-    EventModule,
+    EntityModule.forFeature(
+      [{ name: Event.name, schema: EventSchema }],
+      AppsDbConnectionMap.Planner
+    ),
+  ],
+  controllers: [EventController],
+  providers: [
+    EventService,
+    EventRepository,
+    CreateEventCommandHandler,
+    DeleteEventCommandHandler,
+    FindEventsByUserQueryHandler,
   ],
 })
 export class PlannerModule {}
